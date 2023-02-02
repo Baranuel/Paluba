9fdfa4495ce16f16956c0fb0139ccd6ce4516f26
Baranuel <samuel.baran98@gmail.com>
2023-02-02 15:56:16 +0100

    font size again

--- components/NaseHodnoty/HodnotaCard.tsx
+++ components/NaseHodnoty/HodnotaCard.tsx
@@ -10,19 +10,19 @@ interface HodnotaCardProps {
 
 function HodnotaCard({ image, title, text, isMobile }: HodnotaCardProps) {
   return (
-    <div className=" bg-bgSecondary z-1 p-4 md:p-2 sm:p-4 flex sm:min-w-[40%] sm:max-w-[48%] w-1/5 grow border rounded-lg border-primaryRed items-center">
+    <div className=" bg-bgSecondary z-1 p-4 md:p-2 sm:p-3 flex sm:min-w-[40%] sm:max-w-[48%] w-1/4 grow border rounded-lg border-primaryRed items-center">
       <Image
         className="mr-2"
         src={image}
         alt="hodnota image"
-        width={isMobile ? 40 : 50}
-        height={isMobile ? 40 : 50}
+        width={isMobile ? 35 : 50}
+        height={isMobile ? 30 : 50}
       />
       <div className="min-w-fit flex flex-col  p-2 sm:p-0">
-        <h3 className="font-semibold text-xl lg:text-xl sm:text-[1.1rem] md:text-[1.2rem] sm:leading-[1rem] text-primaryRed">
+        <h3 className="font-semibold text-xl lg:text-xl sm:text-[17px] md:text-md sm:leading-[1rem] text-primaryRed">
           {title}
         </h3>
-        <p className="font-medium text-bluntGrey text-xl lg:text-lg  md:text-[0.75rem] sm:text-[0.85rem] sm:leading-[1.5rem]">
+        <p className="font-medium text-bluntGrey text-xl lg:text-lg  md:text-sm sm:text-[14px] sm:leading-[1.5rem]">
           {text}
         </p>
       </div>
