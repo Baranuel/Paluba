4a607b3c60a73778b9e177680fdf3e50b88ad7d3
Baranuel <samuel.baran98@gmail.com>
2023-02-04 13:14:40 +0100

    copywriting

--- components/NaseHodnoty/HodnotyPage.tsx
+++ components/NaseHodnoty/HodnotyPage.tsx
@@ -16,7 +16,7 @@ interface HodnotyPageProps {
 function HodnotyPage({ isMobile }: HodnotyPageProps) {
   return (
     <div className="bg-bgSecondary z-1 overflow-hidden relative min-h-content pb-12 px-24 2xl:px-64 xl:px-42 md:px-4 sm:px-4 xs:p-2 ">
-      <Heading title="Hodnoty" />
+      <Heading title="Zaručíme vám" />
       <Image
         className="sm:m-auto xs:m-auto absolute -right-40 top-8 sm:inset-0 xs:inset-0 w-1/3 sm:top-0 xs:top-0 h-content xs:w-full xs:h-1/2 sm:w-full sm:h-1/2 object xs:object-center sm:object-center z-0"
         src={bgPic}
@@ -39,7 +39,7 @@ function HodnotyPage({ isMobile }: HodnotyPageProps) {
           isMobile={isMobile}
           image={saladImg}
           title="Kvalita"
-          text="Vždy Fresh"
+          text="Vždy Čerstvé"
         />
         <HodnotaCard
           isMobile={isMobile}
