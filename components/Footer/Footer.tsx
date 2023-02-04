8a64f45a85c3a527dc9d402358f651d6f532fd94
Baranuel <samuel.baran98@gmail.com>
2023-02-04 08:53:35 +0100

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../../assets/PalubaLogo.svg";
+import facebook from "../../assets/facebook.svg";
+import instagram from "../../assets/instagram.svg";
 
 function Footer() {
   return (
-    <footer className="h-44 bg-primaryRed flex">
-      <Image src={logo} alt="logo" width={150} height={150} />
+    <footer className="h-44 bg-primaryRed flex justify-between items-center w-full">
+      <a href="#">
+        <Image src={logo} alt="logo" width={150} height={150} />
+      </a>
+      <div className="h-full flex flex-col text-xl sm:text-sm justify-center items-center w-1/3 text-white">
+        <p className="sm:text-sm">Po-Pia</p>
+        <h4 className="font-semibold">09:30 - 21:30</h4>
+        <p className="sm:text-sm">Sob-Ned</p>
+        <h4 className="font-semibold">09:30 - 21:30</h4>
+      </div>
+      <div className="w-1/3 flex gap-4 justify-center items-center">
+        <Image src={instagram} alt="instagram" width={25} height={25} />
+        <Image src={facebook} alt="facebook" width={15} height={20} />
+      </div>
     </footer>
   );
 }
--- components/Kdenasnajdete/ContactBanner.tsx
+++ components/Kdenasnajdete/ContactBanner.tsx
@@ -50,12 +50,11 @@ function ContactBanner() {
           </div>
         </div>
       </div>
-      <div className=" bg-primaryRed rounded-md w-full border-2 border-primaryRed">
+      <div className=" bg-primaryRed rounded-md w-full border border-primaryRed">
         <div className="google-map-code rounded-md">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1285.198010906577!2d18.842784874975468!3d48.588966667619644!2m3!1f0!2f39.07368641868923!3f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x47152f3ecb252311%3A0x515b05e1214a0c91!2sRozvoz%20Paluba!5e1!3m2!1ssk!2sus!4v1675495771854!5m2!1ssk!2sus"
-            className="w-full h-80 rounded-md border-2 border-primaryRed"
-            style={{ border: 0 }}
+            className="w-full h-80 rounded-md "
             loading="lazy"
           ></iframe>
         </div>
