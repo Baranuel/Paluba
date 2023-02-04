bdcc47e5922870c147f93a736a6093de03667a42
Baranuel <samuel.baran98@gmail.com>
2023-02-04 12:42:38 +0100

    final final touches landing page

--- components/Recenzie/RecenziaCard.tsx
+++ components/Recenzie/RecenziaCard.tsx
@@ -7,11 +7,11 @@ interface RecenziaCardProps {
 
 function RecenziaCard({ text, author }: RecenziaCardProps) {
   return (
-    <div className=" first:self-start last:self-end mt-12 sm:mt-4 p-6 sm:p-4 bg-[#FFF2EC] border-2 rounded-xl flex flex-col border-primaryRed">
+    <div className=" first:self-start last:self-end mt-12 sm:mt-4 p-6 sm:p-4 bg-[#FFF2EC] border rounded-xl flex flex-col border-primaryRed">
       <h1 className="font-rye text-8xl sm:text-5xl self-center flex  text-primaryRed">
         {'"'}
       </h1>
-      <p className=" px-16 md:px-4 sm:px-2 sm:text-sm md:text-lg text-xl text-center font-quicksand font-medium">
+      <p className=" px-16 md:px-4 sm:px-2 sm:text-sm md:text-lg text-xl text-center font-quicksand font-semibold">
         {" "}
         {text}
       </p>
