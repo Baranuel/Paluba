61fdd4627bba75083de16f47b0e8f7ec81163b4b
Baranuel <samuel.baran98@gmail.com>
2023-02-05 18:23:52 +0100

    smaller spinner

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -6,8 +6,8 @@ interface SpinnerProps {
 function Spinner({ loading }: SpinnerProps) {
   return loading ? (
     <div className="fixed flex top-10 z-10 w-full ">
-      <div className="bg-white mx-auto p-3 rounded-xl ">
-        <div className="border-t-transparent border-solid animate-spin mx-auto rounded-full border-primaryRed border-2 h-8 w-8"></div>
+      <div className="bg-white mx-auto p-3 rounded-md ">
+        <div className="border-t-transparent border-solid animate-spin mx-auto rounded-full border-primaryRed border-2 h-6 w-6"></div>
       </div>
     </div>
   ) : null;
