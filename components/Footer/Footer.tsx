b15c6ddd48ebb614911cabe8759a67879b3c1308
Baranuel <samuel.baran98@gmail.com>
2023-02-13 11:04:03 +0100

    headings

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,23 +8,34 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="h-44 bg-primaryRed flex justify-between items-center w-full px-24 2xl:px-64 xl:px-42 md:px-4 sm:px-4 xs:p-2">
-      <Link href="#"  className="w-1/3"passHref >
-  
-        <Image src={logo} alt="logo"style={{width:'150px', height:'150px'}} />
+      <Link href="#" className="w-1/3" passHref>
+        <Image
+          src={logo}
+          alt="logo"
+          style={{ width: "150px", height: "150px" }}
+        />
       </Link>
       <div className="h-full items-center text-center gap-2 flex flex-col text-xl sm:text-sm justify-center items-center w-1/3 text-white">
         <div>
           <p className="sm:text-sm">Po-Pia</p>
-          <h4 className="font-semibold">09:30 - 21:30</h4>
+          <p className="font-semibold">09:30 - 21:30</p>
         </div>
         <div>
           <p className="sm:text-sm">Sob-Ned</p>
-          <h4 className="font-semibold">12:00 - 21:30</h4>
+          <p className="font-semibold">12:00 - 21:30</p>
         </div>
       </div>
       <div className="w-1/3 flex gap-4 justify-center items-center">
-        <Image src={instagram} alt="instagram" style={{width:'25px', height:'25px'}} />
-        <Image src={facebook} alt="facebook" style={{width:'25px', height:'25px'}} />
+        <Image
+          src={instagram}
+          alt="instagram"
+          style={{ width: "25px", height: "25px" }}
+        />
+        <Image
+          src={facebook}
+          alt="facebook"
+          style={{ width: "25px", height: "25px" }}
+        />
       </div>
     </footer>
   );
