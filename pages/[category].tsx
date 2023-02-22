b632e3efd4030befc870f623ea496675f23ada05
Samuel Baran <samuel.baran98@gmail.com>
2023-02-22 14:54:03 +0100

    let's see

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -94,3 +94,10 @@ export async function getStaticProps({ params }: any) {
     revalidate:10
   };
 }
+
+export async function getStaticPaths({params}:any) {
+  return {
+    paths: [{ params: { category: params.category } }],
+    fallback: false, // can also be true or 'blocking'
+  }
+}
